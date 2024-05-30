ab369ad4d36b860b6c2d0df6c9df8c33b12bf6bc
Eesan1 <166600020+Eesan1@users.noreply.github.com>
2024-05-30 20:55:00 +0200

    Update Recipe.cs
    Part 2 has been implemented into part 1

--- Recipe.cs
+++ Recipe.cs
@@ -1,77 +1,115 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace RecipeApp
 {
     class Recipe
     {
-        private string[] ingredients;
-        private double[] quantities;
-        private string[] units;
-        private string[] steps;
+        public string Name { get; private set; }
+        private List<Ingredient> ingredients;
+        private List<string> steps;
+        private List<double> originalQuantities;
 
-        public Recipe()
+        public double TotalCalories => CalculateTotalCalories();
+
+        public Recipe(string name)
         {
-            ingredients = new string[0];
-            quantities = new double[0];
-            units = new string[0];
-            steps = new string[0];
+            Name = name;
+            ingredients = new List<Ingredient>();
+            steps = new List<string>();
+            originalQuantities = new List<double>();
         }
 
-        public void AddIngredient(string name, double quantity, string unit)
+        public void AddIngredient(string name, double quantity, string unit, double calories, FoodGroup foodGroup)
         {
-            Array.Resize(ref ingredients, ingredients.Length + 1);
-            Array.Resize(ref quantities, quantities.Length + 1);
-            Array.Resize(ref units, units.Length + 1);
-
-            int index = ingredients.Length - 1;
-            ingredients[index] = name;
-            quantities[index] = quantity;
-            units[index] = unit;
+            ingredients.Add(new Ingredient(name, quantity, unit, calories, foodGroup));
+            originalQuantities.Add(quantity);
         }
 
         public void AddStep(string step)
         {
-            Array.Resize(ref steps, steps.Length + 1);
-            int index = steps.Length - 1;
-            steps[index] = step;
+            steps.Add(step);
         }
 
         public void Scale(double factor)
         {
-            for (int i = 0; i < quantities.Length; i++)
+            for (int i = 0; i < ingredients.Count; i++)
             {
-                quantities[i] *= factor;
+                ingredients[i].Quantity *= factor;
             }
-        }
-
-        public void Reset()
-        {
-            
         }
 
         public void Clear()
         {
-            ingredients = new string[0];
-            quantities = new double[0];
-            units = new string[0];
-            steps = new string[0];
+            ingredients.Clear();
+            steps.Clear();
+            originalQuantities.Clear();
+        }
+
+        public void ResetQuantities()
+        {
+            for (int i = 0; i < ingredients.Count; i++)
+            {
+                ingredients[i].Quantity = originalQuantities[i];
+            }
+        }
+
+        public double CalculateTotalCalories()
+        {
+                double total = 0;
+                foreach (var ingredient in ingredients)
+                {
+                    total += ingredient.Calories * ingredient.Quantity;
+                }
+                return total;
         }
 
         public override string ToString()
         {
-            string recipeString = "Ingredients:\n";
-            for (int i = 0; i < ingredients.Length; i++)
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Name}");
+            sb.AppendLine("Ingredients:");
+            foreach (var ingredient in ingredients)
             {
-                recipeString += $"{quantities[i]} {units[i]} of {ingredients[i]}\n";
+                sb.AppendLine($"{ingredient.Quantity} {ingredient.Unit} of {ingredient.Name} ({ingredient.Calories} calories, {ingredient.FoodGroup})");
             }
-
-            recipeString += "\nSteps:\n";
-            for (int i = 0; i < steps.Length; i++)
+            sb.AppendLine("\nSteps:");
+            for (int i = 0; i < steps.Count; i++)
             {
-                recipeString += $"{i + 1}. {steps[i]}\n";
+                sb.AppendLine($"{i + 1}. {steps[i]}");
             }
-
-            return recipeString;
+            sb.AppendLine($"\nTotal Calories: {TotalCalories}");
+            return sb.ToString();
         }
+    }
+
+    class Ingredient
+    {
+        public string Name { get; }
+        public double Quantity { get; set; }
+        public string Unit { get; }
+        public double Calories { get; }
+        public FoodGroup FoodGroup { get; }
+
+        public Ingredient(string name, double quantity, string unit, double calories, FoodGroup foodGroup)
+        {
+            Name = name;
+            Quantity = quantity;
+            Unit = unit;
+            Calories = calories;
+            FoodGroup = foodGroup;
+        }
+    }
+
+    enum FoodGroup
+    {
+        Dairy,
+        Protein,
+        Fruit,
+        Vegetable,
+        Grain,
+        Fat,
+        Sugar
     }
 }
